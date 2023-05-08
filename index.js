cc15224e7b5a14f302c91b90dc1457881ffb86de
Meqativ <meqativ@gmail.com>
2023-05-08 13:08:56 +0300

    reword

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ if (args?.values.help || args?.values?.h) {
 				{
 					header: "Vendetta Debugger",
 					content:
-						"A fork of @colin273/enmity-debugger, which is a remote debugger for Vendetta. This connects over a websocket to the Discord app with Vendetta installed and allows you to execute JavaScript in the Discord app from the command line. The REPL in this debugger is a slightly modified version of the default REPL in Node.js, including the same commands and some support for multi-line code snippets. bla bla bla",
+						"A fork of @colin273/enmity-debugger, which is a remote debugger for Vendetta. This connects over a websocket to the Discord app with Vendetta installed and allows you to execute JavaScript in the Discord app from the command line. The REPL in this debugger is a slightly modified version of the default REPL in Node.js, including the same commands and some support for multi-line code snippets",
 				},
 				{
 					header: "Options",
@@ -68,31 +68,31 @@ if (args?.values.help || args?.values?.h) {
 						{
 							name: "help",
 							alias: "h",
-							description: "Shows,, this.",
+							description: "Shows the text you're reading right now",
 							type: Boolean,
 						},
 						{
 							name: "silent",
-							typeLabel: "{underline level} (0-2)",
+							typeLabel: "{underline level} (int, 0-2)",
 							type: String,
 							description:
-								"Level of silency for the output.\n" +
+								"Level of silency for the output\n" +
 								"0: none (default)\n" +
 								"1: hides the welcome message\n" +
-								"2: hides logs from the debugger too",
+								"2: hides logs from the debugger",
 						},
 						{
 							name: "port",
-							typeLabel: "{underline number}",
+							typeLabel: "{underline port} (int, 1-65535)",
 							type: Number,
-							description: "Port on which to run the websocket.",
+							description: "Port on which to run the websocket",
 						},
 						{
 							name: "onConnectedPath",
-							typeLabel: "{underline filePath}",
+							typeLabel: "{underline file_path} (string)",
 							type: String,
 							description:
-								"Path to the file with javascript code that will be sent to the clien t on every connection.",
+								"Path to the file with javascript code, that will be sent to the client on every connection",
 						},
 					],
 				},
